9a5ecd4da5ed766643ff3a41168918d4eb443057
Samuel Mena <samuel23mena@gmail.com>
2020-08-31 23:40:59 -0600

    Docs agregado

---
+++ docs/precache-manifest.e0603e0ef3dfd53c39c1983722b0b54e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cd2a5ca784fe6af676992ca1f57c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960f49bf42ef0f8b8311",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "12245da262874f24e494",
+    "url": "/static/css/main.556e6770.chunk.css"
+  },
+  {
+    "revision": "960f49bf42ef0f8b8311",
+    "url": "/static/js/2.8261f95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8261f95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12245da262874f24e494",
+    "url": "/static/js/main.02b16861.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
